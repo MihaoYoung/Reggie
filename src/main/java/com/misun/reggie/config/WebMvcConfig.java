8ef83949683509b6082c783787852210e01eaa34
Mihao <1794589604@qq.com>
2023-04-18 16:53:31 +0800

    添加：员工信息修改功能+扩展MVC框架的消息转换器

--- src/main/java/com/misun/reggie/config/WebMvcConfig.java
+++ src/main/java/com/misun/reggie/config/WebMvcConfig.java
@@ -1,10 +1,15 @@
 package com.misun.reggie.config;
 
+import com.misun.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -22,5 +27,23 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
         // super.addResourceHandlers(registry);
+    }
+
+    /**
+     * 扩展MVC框架的消息转换器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        // 创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+
+        // 设置对象转换器，底层使用Jackson将Java对象转为JSON
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+
+        // 将消息转换器对象追加到MVC框架的转换器集合中
+        converters.add(0, messageConverter);
+
     }
 }
