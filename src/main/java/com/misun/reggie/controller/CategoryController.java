61ab5f4bf11059239f9b5c183a5813276aa16b68
Mihao <1794589604@qq.com>
2023-04-19 15:59:23 +0800

    添加：修改菜品分类询功能

--- src/main/java/com/misun/reggie/controller/CategoryController.java
+++ src/main/java/com/misun/reggie/controller/CategoryController.java
@@ -50,4 +50,14 @@ public class CategoryController {
 
         return R.success("分类信息删除成功");
     }
+
+    @PutMapping
+    public R<String> update(@RequestBody Category category){
+
+        log.info("修改分类信息：{}", category);
+        categoryService.updateById(category);
+
+        return R.success("分类修改成功");
+
+    }
 }
