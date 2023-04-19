e7ce325e4b6c530d14316e93359b8de4a237f6cc
Mihao <1794589604@qq.com>
2023-04-19 15:00:56 +0800

    添加：菜品分类分页查询功能

--- src/main/java/com/misun/reggie/controller/CategoryController.java
+++ src/main/java/com/misun/reggie/controller/CategoryController.java
@@ -1,14 +1,13 @@
 package com.misun.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.misun.reggie.common.R;
 import com.misun.reggie.entity.Category;
 import com.misun.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -25,5 +24,20 @@ public class CategoryController {
         categoryService.save(category);
 
         return R.success("新增分类成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+
+        log.info("page={}, pageSize={}", page, pageSize);
+
+        Page pageInfo = new Page(page, pageSize);
+
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+
+        queryWrapper.orderByAsc(Category::getSort);
+        categoryService.page(pageInfo, queryWrapper);
+
+        return R.success(pageInfo);
     }
 }
